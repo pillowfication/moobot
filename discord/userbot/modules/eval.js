f25c46b0343fae84c7a47a571011bed5cf4bf878
Markus Tran <thebluepillow@gmail.com>
2017-04-12 14:12:47 -0700

    made eval a vm

--- discord/userbot/modules/eval.js
+++ discord/userbot/modules/eval.js
@@ -1,35 +1,43 @@
-// '_'-prefix variables to prevent accidental `eval` abuse
-const _util = require('util');
-const _winston = require('winston');
+const vm = require('vm');
+const util = require('util');
+const winston = require('winston');
 
 module.exports = {
   init(me) {
-    const _regex = new RegExp(`^${me.prefix}eval\\s+\`\`\`([\\s\\S]*)\`\`\``);
+    const regex = new RegExp(`^${me.prefix}eval\\s+\`\`\`([\\s\\S]*)\`\`\``);
+    const context = vm.createContext(global);
 
     me.on('message', message => {
       if (message.author.id !== me.id || !message.content.startsWith(me.prefix))
         return;
 
-      const _match = message.content.match(_regex);
-      const _code = _match && _match[1];
-      if (!_code)
+      const match = message.content.match(regex);
+      const code = match && match[1];
+      if (!code)
         return;
 
-      let _output;
+      let output;
       try {
-        _output = eval(_code);
+        vm.runInContext('((a,b,c)=>{require=a;me=b;message=c})', context)(require, me, message);
+        output = vm.runInContext(code, context, {
+          filename: 'Discord',
+          lineOffset: 0,
+          columnOffset: 0,
+          displayErrors: true,
+          timeout: 10000
+        });
       } catch (err) {
-        _output = err;
+        output = err;
       }
 
       message
         .edit(
           `${me.prefix}eval` +
-          `\`\`\`js\n${_code}\n\`\`\`` + // Don't put a '\n' here please
-          `\`\`\`js\n${_util.inspect(_output)}\n\`\`\``
+          `\`\`\`js\n${code}\n\`\`\`` +
+          `\`\`\`js\n${util.inspect(output)}\n\`\`\``
         )
         .catch(err =>
-          _winston.error('Could not edit message.', err)
+          winston.error('Could not edit message.', err)
         );
     });
   }
