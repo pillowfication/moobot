55d430cd468590f7c457ed02df677a4e4f71e80f
Markus Tran <thebluepillow@gmail.com>
2017-05-11 12:07:00 -0700

    basic TLMCPlayer

---
+++ discord/moobot/classes/TLMCPlayer.js
@@ -0,0 +1,76 @@
+const request = require('request');
+
+const TLMC_URL = 'http://tlmc.pf-n.co';
+const COUNT = 49207; // TODO: AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+
+function getInfoById(id) {
+  id = String(id).replace(/[^0-9]/g, '');
+
+  return new Promise((resolve, reject) => {
+    request({url: `${TLMC_URL}/tlmc/info/${id}`, json: true}, (err, res, body) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (res.statusCode !== 200) {
+        return new Error(body);
+      }
+
+      resolve({
+        id: id,
+        url: `${TLMC_URL}/tlmc/id/${id}`,
+        info: body
+      });
+    });
+  });
+}
+
+function getInfoRandom() {
+  const index = COUNT * Math.random() | 0;
+  return getInfoById(index + 1);
+}
+
+class TLMCPlayer {
+  constructor(connection) {
+    if (!connection) {
+      throw new TypeError('Parameter `connection` is required');
+    }
+
+    this.connection = connection;
+    this.queue = [];
+  }
+
+  queueTrack(identifier) {
+    return this.constructor.resolveTrack(identifier)
+      .then(info => this.queue.push(info));
+  }
+
+  playNextSong() {
+    let queuedTrack = this.queue.shift();
+    return (queuedTrack
+      ? Promise.resolve(queuedTrack)
+      : this.constructor.resolveTrackRandom())
+      .then(track => this._playSongUrl(track.url))
+      .then(dispatcher => {
+        dispatcher.on('end', this.playNextSong);
+      });
+  }
+
+  _playSongUrl(url) {
+    return this.connection.playArbitraryInput(url);
+  }
+
+  static resolveTrack(identifier) {
+    if (Number.isInteger(Number(identifier))) {
+      return getInfoById(identifier);
+    }
+  }
+
+  static resolveTrackRandom() {
+    return getInfoRandom();
+  }
+}
+
+TLMCPlayer.TLMC_URL = TLMC_URL;
+
+module.exports = TLMCPlayer;
--- discord/moobot/modules/tlmc.js
+++ discord/moobot/modules/tlmc.js
@@ -1,14 +1,7 @@
 const _ = require('lodash');
-const request = require('request');
 const messageError = require('../../messageError');
-const winston = require('winston');
 
-const TLMC_URL = 'http://tlmc.pf-n.co';
-const COUNT = 49207; // TODO: AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
-
-function playError(err) {
-  winston.error('An error occurred trying to stream audio.', err);
-}
+const TLMCPlayer = require('../classes/TLMCPlayer');
 
 module.exports = {
   defaults: {
@@ -19,33 +12,7 @@ module.exports = {
     options = _.defaults(options, module.exports.defaults);
     const command = `${bot.config.prefix}${options.command}`;
 
-    const player = {
-      currentSong: {
-        id: 0,
-        url: null,
-        info: null
-      },
-      queue: [],
-      setCurrentSong: function setCurrentSong(id) {
-        return new Promise((resolve, reject) => {
-          const index = id ? parseInt(id, 10) - 1 : COUNT * Math.random()|0;
-          id = index + 1;
-
-          request.get(`${TLMC_URL}/tlmc/info/${id}`, (err, res, body) => {
-            if (err || res.statusCode !== 200 || !body) {
-              return reject(err || new Error(res.statusMessage));
-            }
-            const song = JSON.parse(body);
-
-            player.currentSong.id = id;
-            player.currentSong.url = `${TLMC_URL}/tlmc/id/${id}`;
-            player.currentSong.info = song;
-
-            resolve(player.currentSong);
-          });
-        });
-      }
-    };
+    const TLMCPlayers = {};
 
     const test = RegExp.prototype.test.bind(new RegExp(`^${command}\\s`));
 
@@ -64,55 +31,33 @@ module.exports = {
           message.channel
             .sendCode('',
               `${command}\n` +
-              `See ${TLMC_URL} for all songs available\n` +
-              '  help            Print this message\n' +
-              '  play <id>       Play a song\n' +
-              '  info            Information about the current song\n' +
-              '  search <query>  TODO\n' +
-              '  queue <id>      TODO\n' +
-              '  skip            TODO'
+              'See http://tlmc.pf-n.co for all songs available\n' +
+              '  help   Print this message\n' +
+              '  start  Join voice channel and initialize player'
             )
             .catch(messageError('send'));
           break;
         }
 
-        case 'play': {
-          const id = tokens[2];
-
-          if (!id) {
-            return message.channel
-              .sendMessage(`No \`id\` specified. See \`${command} help\` for more information.`)
-              .catch(messageError('send'));
-          }
-
+        case 'start': {
           if (!message.member.voiceChannel) {
             return message.channel
               .sendMessage(`You must be in a voice channel. See \`${command} help\` for more information.`)
               .catch(messageError('send'));
           }
 
+          if (TLMCPlayers[message.guild.id]) {
+            return message.channel
+              .sendMessage('Already joined a voice channel.')
+              .catch(messageError('send'));
+          }
+
           message.member.voiceChannel.join()
             .then(connection => {
-              player.setCurrentSong(id)
-                .then(currentSong => {
-                  connection.playArbitraryInput(currentSong.url);
-                  return `Playing #${id} - ${currentSong.info.title}`;
-                })
-                .catch(err => {
-                  playError(err);
-                  return `Error playing \`${id}\`. ${err.message}.`;
-                })
-                .then(msg => message.channel.sendMessage(msg))
-                .catch(messageError('send'));
+              const player = new TLMCPlayer(connection);
+              TLMCPlayers[message.guild.id] = player;
+              player.playNextSong();
             });
-
-          break;
-        }
-
-        case 'info': {
-          message.channel
-            .sendCode('', JSON.stringify(player.currentSong, null, 2))
-            .catch(messageError('send'));
 
           break;
         }
