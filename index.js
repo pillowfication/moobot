ca79b2e7fd0176a6545a34dde00127c439c5acbe
Markus Tran <thebluepillow@gmail.com>
2017-10-24 16:30:53 -0700

    reset

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-require('./src/eorzean')(client)
 require('./src/moo')(client)
-require('./src/userbot')(client)
 
 client.on('ready', () => console.log('moobot ready'))
 client.login(require('./config').discordToken)
--- src/moo/index.js
+++ src/moo/index.js
@@ -1,9 +1,9 @@
 module.exports = function moo (client) {
-  const mooRegex = /\b(?:moo+|cows?)\b/i
+  const mooRegex = RegExp.prototype.test.bind(/\b(?:moo+|cows?)\b/i)
   const probability = 0.2
 
   client.on('message', message => {
-    if (!message.author.bot && Math.random() < probability && mooRegex.test(message.content)) {
+    if (!message.author.bot && Math.random() < probability && mooRegex(message.content)) {
       message.channel.send('moo')
     }
   })
