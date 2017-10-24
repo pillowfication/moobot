1789c6e6fa401e7b89083ff93801b386dacb8d27
Markus Tran <thebluepillow@gmail.com>
2017-10-23 18:25:34 -0700

    i dunno

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
+require('./src/eorzean')(client)
 require('./src/moo')(client)
+require('./src/userbot')(client)
 
 client.on('ready', () => console.log('moobot ready'))
 client.login(require('./config').discordToken)
