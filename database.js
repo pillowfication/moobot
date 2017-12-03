06163936c4ea65249b14feca799b021cdc33ac2b
Markus Tran <thebluepillow@gmail.com>
2017-12-03 01:29:34 -0800

    asdf

--- database.js
+++ database.js
@@ -1,7 +1,6 @@
-
-const scores = {}
 let moos = 0
 
+let scores = {}
 function s (username) {
   scores[username] = (scores[username] || 0) + 1
 }
@@ -17,12 +16,17 @@ module.exports = {
   getScores () {
     const p = []
     for (const a in scores) {
-      p.push({username: a, score: scores[a]})
+      p.push({username: a, score: s[a]})
     }
     return p.sort((a, b) => a.score > b.score ? -1 : 1)
   },
 
   incScore (username) {
     s(username)
+  },
+
+  reset () {
+    moos = 0
+    scores = {}
   }
 }
