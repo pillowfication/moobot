f04527bb0d3f9b072aa87863b88bb8714e3690d3
Markus Tran <thebluepillow@gmail.com>
2018-01-06 14:51:40 -0800

    revert moo prob

--- src/moo.js
+++ src/moo.js
@@ -1,6 +1,6 @@
 module.exports = function moo (client) {
   const mooRegex = RegExp.prototype.test.bind(/\b(?:moo+|cows?)\b/i)
-  const probability = 0.5
+  const probability = 0.2
 
   client.on('message', message => {
     if (!message.author.bot && Math.random() < probability && mooRegex(message.content)) {
