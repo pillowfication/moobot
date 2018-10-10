ce97a0592a58a2af176e02ebc2574e9b6a504f69
Markus Tran <thebluepillow@gmail.com>
2018-10-10 10:27:22 -0700

    brin gback eval

--- src/eval.js
+++ src/eval.js
@@ -18,14 +18,19 @@ module.exports = function _eval (client) {
       getContext(message.channel.id).reset()
       message.channel.send('Context reset.')
     } else if (message.content.startsWith('~/eval')) {
-      const match = message.content.match(/```(?:js\n)?([\s\S]*)```/)
-      if (match) {
-        const context = getContext(message.channel.id)
-        const code = match[1]
-        const result = context.eval(code, { require, message })
-        message.channel.send(result.prettyOutput.substring(0, 100), { code: 'js' })
-      } else {
-        message.channel.send('No code block found to eval.')
+      try {
+        const match = message.content.match(/```(?:(js|javascript)\n)?([\s\S]*)```/)
+        if (match) {
+          const context = getContext(message.channel.id)
+          const code = match[1]
+          const result = context.eval(code, { require, message })
+          message.channel.send(result.prettyOutput.substring(0, 100), { code: 'js' })
+        } else {
+          message.channel.send('No code block found to eval.')
+        }
+      } catch (error) {
+        message.channel.send('Something bad happened. Check logs.')
+        console.error(error)
       }
     }
   })
