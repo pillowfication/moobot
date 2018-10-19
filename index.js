86c6afca538403860e44e70fb00cd4c1918e406f
Markus Tran <thebluepillow@gmail.com>
2018-10-18 21:56:02 -0700

    no more raid

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 require('./src/eorzean')(client)
-// require('./src/eval')(client)
+require('./src/eval')(client)
 require('./src/moo')(client)
-require('./src/raid')(client)
+// require('./src/raid')(client)
 
 client.on('ready', () => console.log('moobot ready'))
 client.login(require('./config').discordToken)
