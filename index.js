ccc3e5042cd9595dcce95deea0b93af127a2eec4
Markus Tran <thebluepillow@gmail.com>
2020-10-20 19:48:24 -0700

    cron is BACK

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-// require('./src/cron')(client)
+require('./src/cron')(client)
 require('./src/eorzean')(client)
 require('./src/eval')(client)
 // require('./src/qd')(client)
